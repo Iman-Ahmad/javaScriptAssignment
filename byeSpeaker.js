d408b358bed91ddf7c50efdda0f138ac0080a7fd
Iman-Ahmad <52351436+Iman-Ahmad@users.noreply.github.com>
2020-04-25 19:00:45 +0300

    Add files via upload

---
+++ byeSpeaker.js
@@ -0,0 +1,7 @@
+var byeSpeaker = new Object();
+
+byeSpeaker.speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
---
+++ helloSpeaker.js
@@ -0,0 +1,8 @@
+var helloSpeaker = new Object();
+
+helloSpeaker.speakWord = "Hello";
+
+
+helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
